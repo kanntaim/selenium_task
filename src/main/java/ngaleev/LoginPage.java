82fc7f45ce9e196f5647caae1852f27a913bb54f
kanntaim <galileo174rus@gmail.com>
2018-11-19 18:50:41 +0300

    stale element exception fixing in progress

--- src/main/java/ngaleev/LoginPage.java
+++ src/main/java/ngaleev/LoginPage.java
@@ -1,7 +1,14 @@
 package ngaleev;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.annotation.Nullable;
+import java.util.function.Predicate;
 
 public class LoginPage {
     private final By loginInputLocator =  By.name("login");
@@ -15,9 +22,17 @@ public class LoginPage {
         }
     }
 
-    public LoginPage authorize(String login, String pass){
-        WebElement txbLogin = driver.findElement(loginInputLocator);
-        txbLogin.sendKeys(login);
+    public LoginPage authorize(final String login, String pass){
+        WebElement txbLogin = new WebDriverWait(driver, 60)
+                .ignoring(StaleElementReferenceException.class)
+                .until(new ExpectedCondition<WebElement>() {
+                    @Override
+                    public boolean apply(@Nullable WebDriver driver) { //FIXME https://stackoverflow.com/questions/12967541/how-to-avoid-staleelementreferenceexception-in-selenium
+                        driver.findElement(loginInputLocator).sendKeys(login);
+                        return true;
+                    }
+                });
+
         txbLogin.submit();
         WebElement txbPassword = driver.findElement(passwordInputLocator);
         txbPassword.sendKeys(pass);
